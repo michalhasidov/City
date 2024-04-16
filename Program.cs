a1a75d2c4e22141495ad92b43df6967d3955f348
michalChasidov <119955290+michalhasidov@users.noreply.github.com>
2024-04-16 14:50:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectCity
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Hello helloForm = new Hello();
+            //Edit editForm = new Edit();
+            //helloForm.ValueSelected += (sender, selectedValue) =>
+            //{
+            //    editForm.UpdateTextBox(selectedValue);
+            //    editForm.Show();
+            //};
+            Application.Run(new Hello());
+             //editForm = new Edit();
+
+        }
+    }
+}
